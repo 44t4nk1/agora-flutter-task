087bc6801c6469f6a42ab35cbacdc22a906ef5df
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2020-12-10 16:20:37 +0530

    initial page improved

--- agoraapp/lib/pages/index.dart
+++ agoraapp/lib/pages/index.dart
@@ -53,7 +53,7 @@ class IndexState extends State<IndexPage> {
             ),
           ),
           SizedBox(
-            height: size.height * 1 / 100,
+            height: size.height * 2 / 100,
           ),
           Container(
             margin: EdgeInsets.symmetric(horizontal: size.width * 4 / 100),
@@ -74,89 +74,72 @@ class IndexState extends State<IndexPage> {
               ),
             ),
           ),
+          SizedBox(
+            height: size.height * 3 / 100,
+          ),
+          ListTile(
+            title: Text("Join as a Broadcaster"),
+            leading: Radio(
+              value: ClientRole.Broadcaster,
+              groupValue: _role,
+              onChanged: (ClientRole value) {
+                setState(() {
+                  _role = value;
+                });
+              },
+            ),
+          ),
+          ListTile(
+            title: Text("Join as a Spectator"),
+            leading: Radio(
+              value: ClientRole.Audience,
+              groupValue: _role,
+              onChanged: (ClientRole value) {
+                setState(() {
+                  _role = value;
+                });
+              },
+            ),
+          ),
+          SizedBox(
+            height: size.height * 5 / 100,
+          ),
+          Container(
+            padding: EdgeInsets.all(size.height * 1 / 100),
+            width: size.width * 40 / 100,
+            decoration: BoxDecoration(
+              color: Colors.blueAccent,
+              borderRadius: BorderRadius.all(
+                Radius.circular(20),
+              ),
+            ),
+            child: FlatButton(
+              onPressed: onJoin,
+              child: Container(
+                child: Text(
+                  'Join',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              color: Colors.blueAccent,
+              textColor: Colors.white,
+            ),
+          ),
         ],
       ),
-      // body: Center(
-      //   child: Container(
-      //     padding: const EdgeInsets.symmetric(horizontal: 20),
-      //     height: 400,
-      //     child: Column(
-      //       children: <Widget>[
-      //         Row(
-      //           children: <Widget>[
-      //             Expanded(
-      //                 child: TextField(
-      //               controller: _channelController,
-      //               decoration: InputDecoration(
-      //                 errorText: _validateError ? 'Channel name is mandatory' : null,
-      //                 border: UnderlineInputBorder(
-      //                   borderSide: BorderSide(width: 1),
-      //                 ),
-      //                 hintText: 'Channel name',
-      //               ),
-      //             ))
-      //           ],
-      //         ),
-      //         Column(
-      //           children: [
-      //             ListTile(
-      //               title: Text("Join as a Broadcaster"),
-      //               leading: Radio(
-      //                 value: ClientRole.Broadcaster,
-      //                 groupValue: _role,
-      //                 onChanged: (ClientRole value) {
-      //                   setState(() {
-      //                     _role = value;
-      //                   });
-      //                 },
-      //               ),
-      //             ),
-      //             ListTile(
-      //               title: Text("Join as a Spectator"),
-      //               leading: Radio(
-      //                 value: ClientRole.Audience,
-      //                 groupValue: _role,
-      //                 onChanged: (ClientRole value) {
-      //                   setState(() {
-      //                     _role = value;
-      //                   });
-      //                 },
-      //               ),
-      //             )
-      //           ],
-      //         ),
-      //         Padding(
-      //           padding: const EdgeInsets.symmetric(vertical: 20),
-      //           child: Row(
-      //             children: <Widget>[
-      //               Expanded(
-      //                 child: RaisedButton(
-      //                   onPressed: onJoin,
-      //                   child: Text('Join'),
-      //                   color: Colors.blueAccent,
-      //                   textColor: Colors.white,
-      //                 ),
-      //               )
-      //             ],
-      //           ),
-      //         )
-      //       ],
-      //     ),
-      //   ),
-      // ),
     );
   }
 
   Future<void> onJoin() async {
-    // update input validation
     setState(() {
       _channelController.text.isEmpty ? _validateError = true : _validateError = false;
     });
     if (_channelController.text.isNotEmpty) {
-      // await for camera and mic permissions before pushing video page
       await _handleCameraAndMic(Permission.camera);
       await _handleCameraAndMic(Permission.microphone);
-      // push video page with given channel name
       await Navigator.push(
         context,
         MaterialPageRoute(
